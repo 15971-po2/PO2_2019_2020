3916dd0939f4e6c5f7fca03428656fd8324263f9
joana <15971@stu.ipbeja.pt>
2020-06-22 02:49:10 +0100

    update

---
+++ moving_rectangle_folder_animation/src/pt/ipbeja/po2/contagious/model/ImmunePerson.java
@@ -0,0 +1,23 @@
+package pt.ipbeja.po2.contagious.model;
+
+public class ImmunePerson extends Person {
+    @Override
+    public boolean isHealthy() {
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean isSick() {
+        return false;
+    }
+
+    @Override
+    public boolean isImmune() {
+        return true;
+    }
+}
